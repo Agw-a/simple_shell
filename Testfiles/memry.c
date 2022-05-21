a54e008dca7f682139283db652f639cddd837f48
Agw-a <lizagwa7@gmail.com>
2022-05-20 21:03:01 -0700

    Args

--- Testfiles/memry.c
+++ Testfiles/memry.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- *_calloc -allocated memory for nmeb size of bytes
+ *_calloc-allocated memory for nmeb size of bytes
  *@nmemb: number of element in the array
- *@size: bytes for each position in the array
+ *@size: bytes for each element in the in the array
  *Return: pointer void
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
